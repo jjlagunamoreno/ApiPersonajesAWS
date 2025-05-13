478b5283bd8464f5a4da8c01c7d1e521c51a1959
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-05-13 11:16:29 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Data/PersonajesContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+using ApiPersonajesAWS.Models;
+
+public class PersonajesContext : DbContext
+{
+    public PersonajesContext(DbContextOptions<PersonajesContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Personaje> Personajes { get; set; }
+}
